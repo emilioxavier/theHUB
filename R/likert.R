22e566239158db1c37f27217b0663dfef0915b39
Emilio Xavier Esposito <emilio.esposito@gmail.com>
2021-11-04 16:33:46 -0400

    added Likert stacked, barplot data.frame construction and plot creation. removed the construction of pairwise heatmap data fxn. removed protocols code

--- R/likert.R
+++ R/likert.R
@@ -57,6 +57,141 @@ convert.fromto <- function(responses, fromto.tb, from, to) {
 }
 
 
+#' @title Construct Likert Stacked, Barplot Data
+#'
+#' @description Construct the Likert data for [likert.barplot.stacked()].
+#'
+#' @param data `tibble` (or `data.frame`) with Likert data converted to integers.
+#'   Use the [convert.fromto()] function to convert text responses to integers.
+#'   _**Note**_: Ensure the integers are positive and non-zero.
+#' @param likert2int.tb `tibble` (or `data.frame`) with the conversion between the
+#'   _**long**_ description and the _**integer**_ value. See [convert.fromto()]
+#'   for an example of the `tibble`'s construction.
+#'
+#' @return `tibble` (or `data.frame`) containing the needed count, percentage, and
+#'   xxxx to construct a Likert stacked, bar chart with [likert.barplot.stacked()].
+#' @export
+#'
+#' @importFrom rlang .data
+#' @importFrom tibble tibble
+#' @importFrom dplyr left_join group_by summarise ungroup mutate select n
+#' @importFrom tidyr pivot_longer pivot_wider separate
+#' @importFrom magrittr %>%
+#'
+#' @author Emilio Xavier Esposito \email{emilio@@msu.edu}
+#'   ([https://github.com/emilioxavier](https://github.com/emilioxavier))
+#'
+make.likert.barplot.data <- function(data, likert2int.tb) {
+
+  ## tibble with range of values ----
+  data.range <- tibble::tibble(Question="RANGE",
+                               Answer=likert2int.tb$integer,
+                               Count=0)
+
+  ## make data long ----
+  data.long <- pivot_longer(data=data,
+                            cols=everything(),
+                            names_to="Question",
+                            values_to="Answer") %>%
+    arrange(Question, Answer) %>%
+    group_by(Question, Answer) %>%
+    summarise(Count=n()) %>%
+    bind_rows(data.range) %>%
+    filter(!is.na(Answer)) %>%
+    ungroup() %>%
+    complete(Question, Answer, fill=list(Count=0)) %>%
+    filter(Question!="RANGE") %>%
+    group_by(Question) %>%
+    mutate(Total=sum(Count),
+           Percent=(Count/Total*100),
+           Label.long=paste(Count, " (", round(Percent, digits=0), "%)", sep=""),
+           pct.label.pos=cumsum(Percent)-0.5*Percent,
+           count.label.pos=cumsum(Count)-0.5*Count
+    ) %>%
+    mutate(Label.long=case_when(Count<=2~"",
+                                TRUE~as.character(Label.long))) %>%
+    ungroup() %>%
+    full_join(y=likert2int.tb, by=c("Answer"="integer")) %>%
+    rename("Answer.txt"="phrase")
+
+  ## add factors for the plotting ----
+  data.long$Question <- factor(x=data.long$Question,
+                               levels=rev(colnames(data)))
+  data.long$Answer <- factor(x=data.long$Answer,
+                             levels=unique(likert2int.tb$integer))
+  data.long$Answer.txt <- factor(x=data.long$Answer.txt,
+                                 levels=unique(likert2int.tb$phrase))
+
+  ## return the plot data ----
+  return(data.long)
+}
+
+
+
+#' @title Construct Pairwise Heatmap Data
+#'
+#' @description Construct the pairwise heatmap data for [likert.heatmap()].
+#'
+#'   This function is called by [likert.heatmap()] but can be called individually.
+#'
+#' @param data `tibble` (or `data.frame`) with Likert data converted to integers.
+#'   Use the [convert.fromto()] function to convert text responses to integers.
+#'   _**Note**_: Ensure the integers are positive and non-zero.
+#' @param QoI string with the Question of Interest (the rows)
+#' @param Qcompared string with the Question being Compared to (the columns)
+#'
+#' @return `tibble` (or `data.frame`) containing the needed count, percentage, and
+#'   colour hex values to construct a heatmap with percentage values overlaid on
+#'   each tile of the heatmap.
+#'
+#' @importFrom rlang .data
+#' @importFrom dplyr left_join group_by summarise ungroup mutate select n
+#' @importFrom tidyr pivot_longer pivot_wider separate
+#' @importFrom magrittr %>%
+#'
+#' @author Emilio Xavier Esposito \email{emilio@@msu.edu}
+#'   ([https://github.com/emilioxavier](https://github.com/emilioxavier))
+#'
+make.likert.heatmap.data <- function(data, QoI, Qcompared, value.range) {
+
+  options(dplyr.summarise.inform=FALSE)
+  data.oi <- select(data, {{QoI}}, {{Qcompared}})
+
+  data.range <- tibble::tibble(!!QoI:=value.range, !!Qcompared:=value.range)
+  data.range <- expand(data.range, .data[[QoI]], .data[[Qcompared]])
+
+  # QvQ.data <- group_by(data.oi, .data[[QoI]], .data[[Qcompared]]) %>%
+  #   summarise("n"=n()) %>%
+  #   ungroup() %>%
+  #   mutate("QoI.n"=sum(n), "QoI.pct"=as.integer(round(n/.data$QoI.n*100, digits=0))) %>%
+  #   select(-"QoI.n", -"n") %>%
+  #   # # pivot_wider(names_from="Q.overall.comm.MSU.leaders", names_prefix="Q2.", values_from="QoI.pct", values_fill=0) %>%
+  #   # pivot_wider(names_from="Q2.ints", names_prefix="Q2.", values_from="QoI.pct", values_fill=0) %>%
+  #   pivot_wider(names_from={{Qcompared}}, names_prefix="Qcompare.", values_from="QoI.pct", values_fill=0) %>%
+  #   pivot_longer(cols=-{{QoI}}, names_to="Q.compared", values_to="pct") %>%
+  #   separate(col="Q.compared", into=c("pre", "Qcompared.ints"), sep="\\.") %>%
+  #   select(-"pre") %>%
+  #   mutate("Qcompared.ints"=as.integer(.data$Qcompared.ints)) %>%
+  #   left_join(msu.heatmap.100, by="pct")
+
+  QvQ.data.1 <- group_by(data.oi, .data[[QoI]], .data[[Qcompared]]) %>%
+    summarise("n"=n()) %>%
+    ungroup()
+
+  QvQ.data <- full_join(x=data.range, y=QvQ.data.1,
+                        by=c({{QoI}}, {{Qcompared}})) %>%
+    replace_na(list(n=0L)) %>%
+    mutate("QoI.n"=sum(n),
+           "pct"=as.integer(round(n/.data$QoI.n*100, digits=0))) %>%
+    left_join(msu.heatmap.100, by="pct") %>%
+    rename("Qcompared.ints"=.data[[Qcompared]]) %>%
+    mutate(hex=case_when(hex=="#E7ECEB"~"#ffffff",
+                         TRUE~as.character(hex)))
+
+  options(dplyr.summarise.inform=TRUE)
+  return(QvQ.data)
+}
+
 
 #' @title Likert (and Likert-like) Barplot
 #'
@@ -138,6 +273,104 @@ likert.barplot <- function(data, QoI, value.range) {
 }
 
 
+#' @title Likert (and Likert-like) Stacked Barplot
+#'
+#' @description Constructs a [ggplot2::] barplot for Likert and Likert-like results
+#'   with each option displayed as an individual, horizontal MSU dark-green bars.
+#'   The percentage of responses are noted on the bar as white text.
+#'
+#'   This function is called by [likert.plot.matrix()] but can be called individually.
+#'
+#' @param data `tibble` (or `data.frame`) with Likert data converted to integers.
+#'   Use the [convert.fromto()] function to convert text responses to integers.
+#'   _**Note**_: Ensure the integers are positive and non-zero.
+#' @param colour.palette the colour palette of interest; default: `"msu.palette"`
+#' @param legend.position to legend's position; default: `"bottom"`
+#' @param label.col string with the column containing the labels. Setting to `NULL`
+#'   will result in no labels.
+#' @param label.size label sizes; default: `4`
+#' @param display.minimum numerical value indicating the small number of responses
+#'   to display a label; default: `3`. Thus, responses with 2 or less responses
+#'   are displayed within the stacked barplot but are not labeled.
+#'
+#' @return ggplot2 graphics object
+#' @export
+#'
+#' @importFrom rlang .data
+#' @importFrom ggplot2 ggplot aes geom_bar labs geom_text after_stat position_stack stat_count
+#' @importFrom cowplot theme_nothing
+#'
+#' @examples
+#' \dontrun{
+#' likert2int.tb <- tibble::tibble(phrase=c("hated it!", "meh", "loved it!"),
+#'                                 integer=c(-1, 0, 1))
+#' make.likert.barplot.data(data, likert2int.tb)
+#' likert.barplot.stacked(data,
+#'                        colour.palette=msu.palette,
+#'                        legend.position="bottom",
+#'                        display.minimum=3)
+#' }
+#'
+#' @author Emilio Xavier Esposito \email{emilio@@msu.edu}
+#'   ([https://github.com/emilioxavier](https://github.com/emilioxavier))
+#'
+likert.barplot.stacked <- function(data,
+                                   # fill,
+                                   colour.palette=msu.palette,
+                                   legend.position="bottom",
+                                   label.col="Label.long",
+                                   label.size=4,
+                                   display.minimum=3) {
+
+  ## plot setup ----
+  n.Answers <- length(unique(data$Answer))
+  colour.values <- rev(colour.palette[1:n.Answers])
+
+  ## caption phrase ----
+  if ( !is.null(display.minimum) ) {
+    display.minimum <- display.minimum - 1
+    caption.txt <- paste("Note: The number and percentage for responses with",
+                         display.minimum,
+                         "or less respondents are not displayed.", sep=" ")
+
+    # data$Label.long[data$Count<=display.minimum] <- ""
+    data[[label.col]][data$Count<=display.minimum] <- ""
+  }
+
+  ## build the barplots ----
+  likert.barplot.stacked <- ggplot(data=data, aes(x=Question, y=Percent, fill=Answer.txt)) +
+    geom_bar(stat="identity", position=position_stack(reverse=TRUE)) +
+    scale_fill_manual(values=rev(msu.palette[1:5]), guide=guide_legend(reverse=FALSE)) +  ## CURRENT
+    labs(x=NULL, y=NULL, title=NULL) +
+    geom_text(aes(y=pct.label.pos, label=.data[[label.col]]), color="white", family="Georgia", size=label.size, hjust="middle", vjust="middle") +
+    coord_flip() +
+    guides(fill=guide_legend(title.position="top",  ## place title on top of legend (not needed)
+                             title.hjust=0.5,  ## center the title (not needed)
+                             label.position="bottom")) +
+    theme_cowplot() +
+    theme(
+      legend.position=legend.position,
+      legend.key.height=unit(0.35, "cm"),
+      legend.key.width=unit(2, "cm"),  ## not used;  see guides()
+      legend.title=element_blank(),
+      legend.text=element_text(size=7),
+      axis.line=element_blank(),
+      axis.ticks=element_blank(),
+      axis.text.x=element_blank(),
+      axis.text.y=element_text(size=14),
+      plot.caption=element_text(size=8))
+
+  ## add caption ----
+  if ( !is.null(display.minimum) ) {
+    likert.barplot.stacked <- likert.barplot.stacked + labs(caption=caption.txt)
+  }
+
+
+  ## return the plot ----
+  return(likert.barplot.stacked)
+}
+
+
 #' @title Construct Likert (and Likert-like) Pairwise Heatmap
 #'
 #' @description Constructs a [ggplot2::] heatmap for Likert and Likert-like results
@@ -188,71 +421,6 @@ likert.heatmap <- function(data, QoI, Qcompared, value.range) {
     #       axis.ticks=element_blank())
 
   return(QvQ.heatmap)
-}
-
-
-#' @title Construct Pairwise Heatmap Data
-#'
-#' @description Construct the pairwise heatmap data for [likert.heatmap()].
-#'
-#'   This function is called by [likert.heatmap()] but can be called individually.
-#'
-#' @param data `tibble` (or `data.frame`) with Likert data converted to integers.
-#'   Use the [convert.fromto()] function to convert text responses to integers.
-#'   _**Note**_: Ensure the integers are positive and non-zero.
-#' @param QoI string with the Question of Interest (the rows)
-#' @param Qcompared string with the Question being Compared to (the columns)
-#'
-#' @return `tibble` (or `data.frame`) containing the needed count, percentage, and
-#'   colour hex values to construct a heatmap with percentage values overlaid on
-#'   each tile of the heatmap.
-#'
-#' @importFrom rlang .data
-#' @importFrom dplyr left_join group_by summarise ungroup mutate select n
-#' @importFrom tidyr pivot_longer pivot_wider separate
-#' @importFrom magrittr %>%
-#'
-#' @author Emilio Xavier Esposito \email{emilio@@msu.edu}
-#'   ([https://github.com/emilioxavier](https://github.com/emilioxavier))
-#'
-make.likert.heatmap.data <- function(data, QoI, Qcompared, value.range) {
-
-  options(dplyr.summarise.inform=FALSE)
-  data.oi <- select(data, {{QoI}}, {{Qcompared}})
-
-  data.range <- tibble::tibble(!!QoI:=value.range, !!Qcompared:=value.range)
-  data.range <- expand(data.range, .data[[QoI]], .data[[Qcompared]])
-
-  # QvQ.data <- group_by(data.oi, .data[[QoI]], .data[[Qcompared]]) %>%
-  #   summarise("n"=n()) %>%
-  #   ungroup() %>%
-  #   mutate("QoI.n"=sum(n), "QoI.pct"=as.integer(round(n/.data$QoI.n*100, digits=0))) %>%
-  #   select(-"QoI.n", -"n") %>%
-  #   # # pivot_wider(names_from="Q.overall.comm.MSU.leaders", names_prefix="Q2.", values_from="QoI.pct", values_fill=0) %>%
-  #   # pivot_wider(names_from="Q2.ints", names_prefix="Q2.", values_from="QoI.pct", values_fill=0) %>%
-  #   pivot_wider(names_from={{Qcompared}}, names_prefix="Qcompare.", values_from="QoI.pct", values_fill=0) %>%
-  #   pivot_longer(cols=-{{QoI}}, names_to="Q.compared", values_to="pct") %>%
-  #   separate(col="Q.compared", into=c("pre", "Qcompared.ints"), sep="\\.") %>%
-  #   select(-"pre") %>%
-  #   mutate("Qcompared.ints"=as.integer(.data$Qcompared.ints)) %>%
-  #   left_join(msu.heatmap.100, by="pct")
-
-  QvQ.data.1 <- group_by(data.oi, .data[[QoI]], .data[[Qcompared]]) %>%
-    summarise("n"=n()) %>%
-    ungroup()
-
-  QvQ.data <- full_join(x=data.range, y=QvQ.data.1,
-                        by=c({{QoI}}, {{Qcompared}})) %>%
-    replace_na(list(n=0L)) %>%
-    mutate("QoI.n"=sum(n),
-           "pct"=as.integer(round(n/.data$QoI.n*100, digits=0))) %>%
-    left_join(msu.heatmap.100, by="pct") %>%
-    rename("Qcompared.ints"=.data[[Qcompared]]) %>%
-    mutate(hex=case_when(hex=="#E7ECEB"~"#ffffff",
-                         TRUE~as.character(hex)))
-
-  options(dplyr.summarise.inform=TRUE)
-  return(QvQ.data)
 }
 
 
@@ -416,103 +584,3 @@ likert.plot.matrix <- function(data, value.range, title, questions, footnotes=NU
   return(the.plot)
 
 }
-
-
-## /\/\/\/\ FUNCTIONS ----
-## \/\/\/\/ PROTOCOLS ----
-#
-# library(tidyverse)
-# library(tidytext)
-# library(ggwordcloud)
-# library(tm)
-# library(ggraph)
-# library(igraph)
-# library(cowplot)
-# library(ggstar)
-# library(GGally)
-#
-# load(file="./data-raw/theHUB_datasets.RData")
-#
-# short <- likert.general[rowSums(is.na.data.frame(likert.general)) == 0, c(4,5,8)]
-#
-# phrase2int.tb <- tibble::tibble(phrase=c("Extremely dissatisfied", "Somewhat dissatisfied", "Neither satisfied nor dissatisfied", "Somewhat satisfied", "Extremely satisfied"),
-#                                 integer=c(1,2,3,4,5))
-# phrase2int.online.tb <- tibble::tibble(phrase=c("No, this was my first experience in fully online instruction",
-#                                                 "Yes, I've taken one or two fully online courses",
-#                                                 "Yes, I've taken many fully online courses"),
-#                                 integer=c(1,2,3))
-# short.ints <- mutate(short,
-#                      Q1.ints=convert.fromto(responses=Q1.overall.MSU, fromto.tb=phrase2int.tb, from="phrase", to="integer"),
-#                      Q2.ints=convert.fromto(responses=Q2.overall.comm.MSU.leaders, fromto.tb=phrase2int.tb, from="phrase", to="integer"),
-#                      Q5.ints=convert.fromto(responses=Q5.online.before, fromto.tb=phrase2int.online.tb, from="phrase", to="integer")
-# )
-#
-#
-# likert.ints <- select(short.ints, ends_with("ints"))
-#
-#
-#
-#
-#
-# ## pairwise likert analysis ----
-#
-#
-# select(likert.general.ints, ends_with(".ints")) %>%
-#   group_by(Q1.ints) %>%
-#   summarise(blah=mean(Q2.ints))
-#
-#
-#
-# ## pairwise multi-response analysis ----
-# compare.dataset(data=select(multi.general, starts_with("Q.")))
-#
-#
-#
-#
-#
-# ggplot(likes.ints, aes(x=Q.overall.MSU, y=Q.online.before, fill=..count..)) +
-#   geom_bin2d()
-#
-# likert.count.long <- pivot_longer(likert.counts,
-#              cols=starts_with("Q.overall"),
-#              names_to="Questions",
-#              values_to="Counts") %>%
-#   arrange(Questions) %>%
-#   group_by(Questions) %>%
-#   mutate(Total.Replies=sum(Counts)) %>%
-#   ungroup() %>%
-#   mutate(Ratios=Counts/Total.Replies,
-#          Percents=Ratios*100,
-#          Labels=paste0(round(Percents, digits=0), "%"))
-#
-# ggpairs(likert.ints,
-#         columns=c("Q1.ints", "Q2.ints", "Q5.ints"))
-#
-#
-#
-# yad <- group_by(likes.ints, Q.overall.MSU, Q.overall.comm.MSU.leaders, Q.online.before) %>%
-#   summarise(n=n()) %>%
-#   pivot_longer(cols=Q.overall.comm.MSU.leaders, names_to="question", values_to="response") %>%
-#   group_by(Q.overall.MSU) %>%
-#   mutate(Q1.resp.n=sum(n)) %>%
-#   ungroup() %>%
-#   group_by(Q.online.before) %>%
-#   mutate(Q5.resp.tot=sum(n))
-#
-#
-# # group_by(likes, Q.overall.MSU, Q.overall.comm.MSU.leaders, Q.overall.comm.instr, Q.overall.train.support.OL, Q.online.before) %>%
-# response.summary <- group_by(likes.ints, Q.overall.MSU, Q.overall.comm.MSU.leaders, Q.online.before) %>%
-#   summarise(n=n()) %>%
-#   pivot_wider(names_from=Q.online.before, values_from=n, values_fill=0, names_prefix="r.") %>%
-#   ungroup()
-#
-# # response.summary <- group_by(likes.ints, Q.overall.MSU, Q.online.before) %>%
-# group_by(short, select(short, everything())) %>%
-#   summarise(n=n()) %>%
-#   pivot_longer(cols=starts_with("Q"), names_to="question", values_to="response")
-#
-#
-# response.colSums <- colSums(select(response.summary, starts_with("r.")))
-# response.rowSums <- rowSums(select(response.summary, starts_with("r.")))
-#
-# select(response.summary, starts_with("r."))
