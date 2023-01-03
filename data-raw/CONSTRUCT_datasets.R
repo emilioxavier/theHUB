255a91887f28527603d9f15044ff3139f8529f4c
emilioxavier <emilio.esposito@gmail.com>
2023-01-03 14:06:07 -0500

    renamed Excel files containing datasets so that all the Excel datasets files are listed together when sorted alphabetically.
    satisfies issue #6 "add the PAC-12 institutions"
    satisfies issue #7 "add the Western Michigan Peer Institution dataset"
    satisfies issue #8 "create an institutions of interest dataset"

--- data-raw/CONSTRUCT_datasets.R
+++ data-raw/CONSTRUCT_datasets.R
@@ -1,18 +1,33 @@
 library(tidyverse)
 library(readxl)
+library(WriteXLS)
 
+
+## load post-secondary education dataset ----
+load(file="~/OneDrive - Michigan State University/projects/PAG/DATASETS_CLEAN/NCES_HighSchools-and-PostSecondary_DATASET.RData")
+
+PostSecondary.2020.2021 <- filter(PostSecondary, SCHOOLYEAR=="2020-2021") |>
+  select(UNITID, LAT, LON)
 
 ## institutions of interest dataset ----
-MSUpeers <- readxl::read_excel(path="data-raw/theHUB_datasets.xlsx",
-                               sheet="MSUpeers")
-
-
+MSUpeers <- readxl::read_excel(path="data-raw/datasets_theHUB.xlsx",
+                               sheet="MSUpeers") |>
+  mutate(UNITID=as.character(UNITID)) |>
+  left_join(y=PostSecondary.2020.2021, by="UNITID") |>
+  select(INSTNM:ZIP, LAT, LON, everything())
 
 ## Western Michigan University peer institutions ----
-WMUpeers <- readxl::read_excel(path="data-raw/theHUB_datasets.xlsx",
-                               sheet="WMUpeers")
-
+WMUpeers <- readxl::read_excel(path="data-raw/datasets_theHUB.xlsx",
+                               sheet="WMUpeers") |>
+  mutate(UNITID=as.character(UNITID)) |>
+left_join(y=PostSecondary.2020.2021, by="UNITID") |>
+  select(INSTNM:ZIP, LAT, LON, everything())
 
 ## save datasets ----
 usethis::use_data(MSUpeers, WMUpeers,
                   internal=FALSE, overwrite=TRUE)
+
+WriteXLS::WriteXLS(x=c("MSUpeers", "WMUpeers"),
+                   ExcelFileName="./data-raw/datasets_PeerInstitution.xlsx",
+                   SheetNames=c("MSUpeers", "WMUpeers"),
+                   FreezeRow=1, FreezeCol=1)
