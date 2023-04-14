627ede90d2b074be2d52f676078556a369f5c29b
emilioxavier <emilio.esposito@gmail.com>
2023-04-14 08:54:28 -0400

    updated documentation of the Country & Currency dataset. updated how the dataset is constructed

--- vignettes/Datasets_CountryCurrency.R
+++ vignettes/Datasets_CountryCurrency.R
@@ -4,9 +4,44 @@
 #    comment = "#>"
 #  )
 #  library(knitr)
+#  library(kableExtra)
 
-## ----loadPackages-------------------------------------------------------------
-library(theHUB)
-library(ISOcodes)
-library(priceR)
+## ----loadPackages, eval=FALSE, include=TRUE-----------------------------------
+#  library(theHUB)
+#  library(ISOcodes)
+#  library(priceR)
+
+## ----ExtractCountry, eval=FALSE, include=TRUE---------------------------------
+#  country.iso <- as_tibble(ISOcodes::ISO_3166_1) |>
+#    mutate(Name.ISO=Name)
+
+## ----ExtractCurrency, eval=FALSE, include=TRUE--------------------------------
+#  country.currencies <- priceR::currencies() |>
+#    as_tibble() |>
+#    separate(col="code", into=c("Alpha_2", "currency.DROP"), remove=FALSE, sep="[[:alpha:]]{1}$") |>
+#    select(-currency.DROP) |>
+#    rename("currency"="description",
+#           "currency.code"="code")
+
+## ----MergeEverything, eval=FALSE, include=TRUE--------------------------------
+#  country.DATA <- full_join(x=country.iso, y=country.currencies, by="Alpha_2") |>
+#    full_join(y=SLATE.country.names, by="Name")
+
+## ----WriteXLSX, eval=FALSE, include=TRUE--------------------------------------
+#  WriteXLS::WriteXLS(x="country.DATA",
+#                     ExcelFileName="CountryISO-slate_matches.xlsx",
+#                     FreezeRow=1)
+
+## ----loadPackages2, include=FALSE---------------------------------------------
+library(tidyselect)
+library(dplyr)
+library(tidyr)
+
+## ----theDataset, echo=FALSE---------------------------------------------------
+country.currency.DISPLAY <- select(theHUB::country.currency, -Common_name, -contains("SLATE")) |>
+  mutate(across(where(is.character), ~replace_na(.x, "")))
+col.names <- c("Alpha 2", "Alpha 3", "Numeric", "Name", "Official Name", "Name (ISO)", "Currency", "Currency Code")
+
+kableExtra::kbl(country.currency.DISPLAY, booktabs=TRUE, caption=NULL, col.names=col.names) |> 
+  kableExtra::kable_styling(latex_options="striped")
 
